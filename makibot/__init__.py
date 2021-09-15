7ae31809c08afa4a847724f48b423ebed0ead9fa
Mauricio Zanetti Filho <mzfshark@gmail.com>
2021-09-15 18:10:38 -0300

    update config
    add

--- makibot/__init__.py
+++ makibot/__init__.py
@@ -59,7 +59,7 @@ if ENV:
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
 
 else:
-    from tg_bot.config import Development as Config
+    from makibot.config import Development as Config
     TOKEN = Config.API_KEY
     try:
         OWNER_ID = int(Config.OWNER_ID)
@@ -112,7 +112,7 @@ WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
 
 # Load at end to ensure all prev variables have been set
-from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
+from makibot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
 
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
