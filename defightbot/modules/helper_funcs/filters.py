b56f4132801a3cdfc74d4e89f900e0f9e789c346
Mauricio Zanetti Filho <admin@whostler.com>
2022-01-19 17:59:43 -0300

    done

--- defightbot/modules/helper_funcs/filters.py
+++ defightbot/modules/helper_funcs/filters.py
@@ -1,7 +1,7 @@
 from telegram import Message
 from telegram.ext import BaseFilter
 
-from makibot import SUPPORT_USERS, SUDO_USERS
+from defightbot import SUPPORT_USERS, SUDO_USERS
 
 
 class CustomFilters(object):
